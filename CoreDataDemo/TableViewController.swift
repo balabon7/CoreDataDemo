4c9c6a951596f17351a2863f5327d3ee50ed6189
Alex Balabon <balabon7@gmail.com>
2020-06-26 17:59:40 +0300

    Finishing to-do list app

--- CoreDataDemo/TableViewController.swift
+++ CoreDataDemo/TableViewController.swift
@@ -10,36 +10,14 @@ import UIKit
 import CoreData
 
 class TableViewController: UITableViewController {
-
+    
     var toDoItems: [Task] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    @IBAction func addTask(_ sender: UIBarButtonItem) {
-        
-        let alertController = UIAlertController(title: "Add Task", message: "add new task", preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-            let textField = alertController.textFields?[0]
-            self.saveTask(taskToDo: (textField?.text)!)
-    
-            self.tableView.reloadData()
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
-        alertController.addTextField { (textField) in
-            
-        }
-        alertController.addAction(okAction)
-        alertController.addAction(cancelAction)
-        
-        present(alertController, animated: true)
-    }
     
     private func saveTask(taskToDo: String) {
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let contex = appDelegate.persistentContainer.viewContext
+        
         let entity = NSEntityDescription.entity(forEntityName: "Task", in: contex)
         let taskObject = NSManagedObject(entity: entity!, insertInto: contex) as! Task
         taskObject.taskToDo = taskToDo
@@ -53,13 +31,53 @@ class TableViewController: UITableViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let contex = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            toDoItems = try contex.fetch(fetchRequest)
+        } catch {
+            print("error", error.localizedDescription)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func addTask(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "Add Task", message: "add new task", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            let textField = alertController.textFields?[0]
+            self.saveTask(taskToDo: (textField?.text)!)
+            
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addTextField { (textField) in
+            
+        }
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true)
+    }
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return toDoItems.count
@@ -72,5 +90,5 @@ class TableViewController: UITableViewController {
         cell.textLabel?.text = task.taskToDo
         return cell
     }
-
+    
 }
